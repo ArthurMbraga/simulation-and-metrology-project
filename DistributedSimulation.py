82102618ff40b078d9121c2750b2dfa2477746ac
Arthur Martins Braga <arthur.martinsbraga@telecom-paris.fr>
2024-12-10 18:02:58 +0100

    Start to distribute

---
+++ DistributedSimulation.py
@@ -0,0 +1,143 @@
+import getpass
+import os
+import paramiko
+from paramiko import SSHClient
+from scp import SCPClient
+
+# Prompt for the login
+login = input("Enter login: ")
+
+# Prompt for the password securely
+password = getpass.getpass("Enter password for {}: ".format(login))
+
+metrics_file_name = "results.csv"
+node_project_name = "server"
+master_project_name = "master"
+file_suffix = "-1.0-jar-with-dependencies.jar"
+remote_folder = "/tmp/{}/".format(login)
+
+# List of computers
+computers = [
+    "tp-1a201-01", "tp-1a201-03", "tp-1a201-04", "tp-1a201-16", "tp-1a201-17",
+    "tp-1a201-18", "tp-1a201-19", "tp-1a201-20", "tp-1a201-21", "tp-1a201-23",
+    "tp-1a201-25", "tp-1a201-26", "tp-1a201-27", "tp-1a201-28", "tp-1a201-29",
+    "tp-1a201-30", "tp-1a201-31", "tp-1a201-32", "tp-1a201-33", "tp-1a201-34",
+    "tp-1a201-36", "tp-1a201-37", "tp-1a201-38", "tp-1a201-39", "tp-1a207-01",
+    "tp-1a207-02", "tp-1a207-03", "tp-1a207-04", "tp-1a207-05", "tp-1a207-06",
+    "tp-1a207-07", "tp-1a207-08", "tp-1a207-11", "tp-1a207-12", "tp-1a207-13",
+    "tp-1a207-14", "tp-1a207-15", "tp-1a207-19", "tp-1a207-17", "tp-1a207-18",
+]
+master = "tp-m5-09"
+
+# Clear the terminal
+os.system("clear")
+
+allComputers = computers + [master]
+
+# Check if there is some repeated computer
+if len(allComputers) != len(set(allComputers)):
+    print("There are repeated computers")
+    exit(0)
+
+# Attach the terminal to the process and print all output until java process ends
+
+
+def attach_and_run_master(ssh, ips, amount_of_data):
+    stdin, stdout, stderr = ssh.exec_command(
+        "cd {}; java -Xms8g -Xmx10g -jar {}{} {} {}".format(
+            remote_folder, master_project_name, file_suffix, ",".join(ips), amount_of_data))
+
+    # Print all output and error messages
+    count = 0
+    while not stdout.channel.exit_status_ready():
+        for line in stdout:
+            count += 1
+            print(line, end="")
+            if (count > 10000):
+                break
+
+        for line in stderr:
+            count += 1
+            print(line, end="")
+            if (count > 10000):
+                break
+
+    for line in stderr:
+        print(line, end="")
+
+    # Update local metrics file
+    with SCPClient(ssh.get_transport()) as scp:
+        scp.get("{}{}".format(
+            remote_folder, metrics_file_name), "./")
+
+
+index = -1
+for c in allComputers:
+    try:
+        index += 1
+        ssh = SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(c, username=login, password=password)
+
+        ssh.exec_command("pkill -u {}".format(login))
+
+        ssh.connect(c, username=login, password=password)
+
+        project_name = master_project_name if c == master else node_project_name
+
+        # Remove and recreate the remote folder
+        ssh.exec_command("rm -rf {}".format(remote_folder))
+        ssh.exec_command("mkdir -p {}".format(remote_folder))
+
+        # Copy the jar file to the remote folder
+        with SCPClient(ssh.get_transport()) as scp:
+            scp.put("./{}/target/{}{}".format(project_name, project_name, file_suffix),
+                    "{}{}{}".format(remote_folder, project_name, file_suffix))
+
+        if c != master:
+            ssh.exec_command(
+                "cd {}; java -Xms8g -Xmx10g -jar {}{}".format(remote_folder, project_name, file_suffix))
+            print("Successfully deployed on {} ({})".format(c, index))
+
+        else:
+            print("Deploying master")
+            # if is master add a small delay to ensure that all nodes are ready
+            ssh.exec_command("sleep 1")
+
+            # Varying number of nodes
+            for v in [0.1, 0.05, 0.01]:
+                for i in range(1, len(computers)):
+                    attach_and_run_master(ssh, computers[:i], v)
+
+            # Varying amount of data
+            for percentage in range(1, 11):
+                print("Running with {}% of the data".format(percentage*10))
+                attach_and_run_master(
+                    ssh, computers[:len(computers)//3], percentage/10)
+
+            # delete old results.csv
+            ssh.exec_command("rm -rf {}".format(remote_folder))
+
+    except paramiko.AuthenticationException:
+        print("Authentication failed for {}".format(c))
+        break
+    except paramiko.SSHException as ssh_exception:
+        print("SSH connection failed for {}: {}".format(c, str(ssh_exception)))
+    finally:
+        ssh.close()
+
+
+# Kill all processes
+for c in allComputers:
+    try:
+        ssh = SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(c, username=login, password=password)
+        ssh.exec_command("pkill -u {}".format(login))
+    except paramiko.AuthenticationException:
+        print("Authentication failed for {}".format(c))
+        break
+    except paramiko.SSHException as ssh_exception:
+        print("SSH connection failed for {}: {}".format(c, str(ssh_exception)))
+    finally:
+        ssh.close()
--- Simulation.py
+++ Simulation.py
@@ -10,57 +10,51 @@ from tqdm import tqdm
 
 
 class Simulation:
-    def run_simulation(self, burstiness_values, simulation_time):
-        total_simulations = len(burstiness_values)
-        overall_progress = tqdm(total=total_simulations,
-                                desc="Overall Progress")
+    @staticmethod
+    def run_simulation(burstiness, simulation_time, periodPrintLR, blockSize):
+        env = simpy.Environment()
 
-        for burstiness in burstiness_values:
-            env = simpy.Environment()
+        Globals.periodPrintLR = periodPrintLR
+        Globals.blockSize = blockSize
 
-            Globals.periodPrintLR = 10**3
-            Globals.blockSize = 10**1
+        q = QueueClass(env, serviceRate=100 * 10**6)
 
-            q = QueueClass(env, serviceRate=100 * 10**6)
+        # Remove the sources from the dataframes
+        Globals.df_responseTimes = pd.DataFrame(
+            columns=['sourceId', 'time', 'responseTime'])
+        Globals.df_blockRespTimes = pd.DataFrame(
+            columns=['sourceId', 'time', 'responseTime', 'epsilon_T'])
+        Globals.sources = {}
 
-            # Remove the sources from the dataframes
-            Globals.df_responseTimes = pd.DataFrame(
-                columns=['sourceId', 'time', 'responseTime'])
-            Globals.df_blockRespTimes = pd.DataFrame(
-                columns=['sourceId', 'time', 'responseTime', 'epsilon_T'])
-            Globals.sources = {}
+        df_responseTimes = Globals.df_responseTimes
+        df_blockRespTimes = Globals.df_blockRespTimes
 
-            df_responseTimes = Globals.df_responseTimes
-            df_blockRespTimes = Globals.df_blockRespTimes
+        dataSource = PoissonSource(
+            env, q, ident=1, transmissionRate=30 * 10**6)
+        Globals.sources[dataSource.ident] = dataSource
 
-            dataSource = PoissonSource(
-                env, q, ident=1, transmissionRate=30 * 10**6)
-            Globals.sources[dataSource.ident] = dataSource
+        voiceSource = ConstantSource(
+            env, q, ident=2, transmissionRate=20 * 10**6, packetSize=800)
+        Globals.sources[voiceSource.ident] = voiceSource
 
-            voiceSource = ConstantSource(
-                env, q, ident=2, transmissionRate=20 * 10**6, packetSize=800)
-            Globals.sources[voiceSource.ident] = voiceSource
+        videoSource = BurstySource(env, q, ident=3, transmissionRate=30 * 10**6,
+                                   packetSize=8000, avgPeriodOn=10**-3, burstiness=burstiness)
+        Globals.sources[videoSource.ident] = videoSource
 
-            videoSource = BurstySource(env, q, ident=3, transmissionRate=30 * 10**6,
-                                       packetSize=8000, avgPeriodOn=10**-3, burstiness=burstiness)
-            Globals.sources[videoSource.ident] = videoSource
+        simulation_progress = tqdm(
+            total=simulation_time, desc=f"Simulation Burstiness {burstiness}")
+        env.process(Simulation.progress_bar(env, simulation_progress))
+        env.run(until=simulation_time)
+        simulation_progress.close()
 
-            simulation_progress = tqdm(
-                total=simulation_time, desc=f"Simulation Burstiness {burstiness}")
-            env.process(self.progress_bar(env, simulation_progress))
-            env.run(until=simulation_time)
-            simulation_progress.close()
+        df_responseTimes['burstiness'] = burstiness
+        df_blockRespTimes['burstiness'] = burstiness
 
-            df_responseTimes['burstiness'] = burstiness
-            df_blockRespTimes['burstiness'] = burstiness
+        Simulation.save_results(
+            df_responseTimes, df_blockRespTimes, burstiness)
 
-            self.save_results(df_responseTimes, df_blockRespTimes, burstiness)
-
-            overall_progress.update(1)
-
-        overall_progress.close()
-
-    def save_results(self, df_responseTimes, df_blockRespTimes, burstiness):
+    @staticmethod
+    def save_results(df_responseTimes, df_blockRespTimes, burstiness):
         os.makedirs('results', exist_ok=True)
         response_times_file = f'results/response_times_burstiness_{burstiness}.csv'
         block_response_times_file = f'results/block_response_times_burstiness_{burstiness}.csv'
@@ -68,15 +62,25 @@ class Simulation:
         df_responseTimes.to_csv(response_times_file, index=False)
         df_blockRespTimes.to_csv(block_response_times_file, index=False)
 
-    def progress_bar(self, env, progress):
+    @staticmethod
+    def progress_bar(env, progress):
         while True:
             progress.update(1)
             yield env.timeout(1)
 
 
 if __name__ == "__main__":
-    simulation = Simulation()
+    burstiness_values = [1, 2, 5, 10, 20, 30, 70, 100]
+    simulation_time = 10**4
+    periodPrintLR = 10**3
+    blockSize = 10**1
 
-    burstiness_values = [1, 2, 5, 10, 20]
-    simulation_time = 10**1 / 2
-    simulation.run_simulation(burstiness_values, simulation_time)
+    total_simulations = len(burstiness_values)
+    overall_progress = tqdm(total=total_simulations, desc="Overall Progress")
+
+    for burstiness in burstiness_values:
+        Simulation.run_simulation(
+            burstiness, simulation_time, periodPrintLR, blockSize)
+        overall_progress.update(1)
+
+    overall_progress.close()
